5213148816acf5b403b7d395ba9c325ec02be150
“mahfuz-7148” <mrahman7148@gmail.com>
2024-10-28 00:49:46 +0600

    running

---
+++ script.js
@@ -0,0 +1,5 @@
+const weatherForm = document.querySelector('.weatherForm')
+const cityInput = document.querySelector('.cityInput')
+const card = document.querySelector('.card')
+const api = '35c5eb672cb43f8a6148140a7f67605e'
+
